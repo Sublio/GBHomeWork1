a4ff3a4323f264863b216eac21f336260351f98f
Denis Mordvinov <denis.mordvinov@corp.badoo.com>
2020-11-24 21:56:51 +0000

    Second task

--- 1l_MordvinovDenis.playground/Contents.swift
+++ 1l_MordvinovDenis.playground/Contents.swift
@@ -1,4 +1,5 @@
-import UIKit
+import Foundation
+////////////////////////////////////////////////////////////////////////////////////////////////////////
 // quadratic equation
 // x2 − 8x + 12 = 0
 // b*2 - 4ac
@@ -7,3 +8,14 @@ let discrim = (b2 - 4*(1*12)) // discriminant is more than 0 -> two answers
 let discrimInt = (discrim as NSDecimalNumber).doubleValue // Cast Decimal to Double as Decimal doesnt have squareRoot method
 let x1 = (-8 + discrimInt.squareRoot())/2*1
 let x2 = (-8 - discrimInt.squareRoot())/2*1
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Right Triangle Area
+let leg1 = 8
+let leg2 = 6
+let triangleArea = 0.5 * Float(leg1) * Float(leg2)
+
+// Right Triangle Perimetr
+
+let hypotenuse = ((pow(Decimal(leg1), 2) + pow(Decimal(leg2), 2)) as NSDecimalNumber).doubleValue.squareRoot()
+let perimetr = leg1 + leg2 + Int(hypotenuse)
