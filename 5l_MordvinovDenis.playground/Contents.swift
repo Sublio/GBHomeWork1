e0e4c7307a70acc8133ca06404e9792a52b172de
Denis Mordvinov <denis.mordvinov@corp.badoo.com>
2020-12-10 02:42:32 +0000

    Final version

--- 5l_MordvinovDenis.playground/Contents.swift
+++ 5l_MordvinovDenis.playground/Contents.swift
@@ -1,151 +1,86 @@
-import Foundation
-
-protocol MainCar {
-    var currentSpeed:Int      { get set }
-    var bodyType:String       { get set }
-    var engineType: String    { get set }
-    var windowOpened: Bool    { get set }
-    var engineIsOn: Bool      { get set }
-    var selectedGear: Int     { get set }
-    
-    func action(action: CarAction)
-    
+protocol Car    {
+    var speed: Int { get }
+    var bodyType: String { get }
+    var windowOpened:Bool { get set }
+    var isEngineOn: Bool { get set }
 }
 
-extension MainCar {
+extension Car{
+    mutating func openWindow(){
+        self.windowOpened = true
+    }
     
-    mutating func action(action: CarAction) {
-        print("Action from protocol extension was caled")
-        switch action {
-        case .openWindow:
-            windowOpened = true
-        case .closeWindow:
-            self.windowOpened = false
-        case .turnEngineOn:
-            self.engineIsOn = true
-        case .turnEngineOff:
-            self.engineIsOn = false
-        case .setBodyType(let bodyType):
-            self.bodyType = bodyType
-        case .setGear(let gear):
-            self.selectedGear = gear
-        case .setSpeed(let speed):
-            self.currentSpeed = speed
-        case .setEngineType(let engine):
-            self.engineType = engine
-        default:
-            print("Default case called")
-        }
+    mutating func closeWindow(){
+        self.windowOpened = false
+    }
+    
+    mutating func startEngine(){
+        self.isEngineOn = true
+    }
+    
+    mutating func stopEngine(){
+        self.isEngineOn = false
     }
 }
 
-
-enum CarAction {
-    case turnEngineOn
-    case turnEngineOff
-    case openWindow
-    case closeWindow
-    case setBodyType(bodyType:String)
-    case setSpeed(speed:Int)
-    case setGear(gear:Int)
-    case setEngineType(type: String)
-    // Actions for specific car types
-    case hookTrailer // For truck
-    case turnOnSpeedLimiter // For sport car
-}
-
-class SportCar: MainCar {
-    func action(action: CarAction) {
-        switch action {
-            case .turnOnSpeedLimiter:
-                self.isSpeedLimiterOn = true
-            default:
-                print("Default case was called")
-        }
-    }
-    
-    
-    var currentSpeed: Int = 0
-    
-    var bodyType: String = ""
-    
-    var engineType: String = ""
-    
+class SportCar: Car {
+    var isEngineOn: Bool
     var windowOpened: Bool
+    var speed: Int
+    var bodyType: String
+    var speedLimiterEnabled: Bool // Property for sportCar only
     
-    var engineIsOn: Bool
-    
-    var selectedGear: Int = 0
-    
-    // Specific Property for SportCar
-    
-    var isSpeedLimiterOn: Bool
-    
-    init(currentSpeed: Int, bodyType: String, engineType: String, windowOpened: Bool, engineIsOn: Bool, selectedGear: Int, isSpeedLimiterOn:Bool ) {
-        self.currentSpeed = currentSpeed
+    init(speed: Int, bodyType:String, windowOpened: Bool, isEngineOn: Bool, speedLimiterEnabled: Bool){
+        self.speed = speed
         self.bodyType = bodyType
-        self.engineType = engineType
         self.windowOpened = windowOpened
-        self.engineIsOn = engineIsOn
-        self.selectedGear = selectedGear
-        self.isSpeedLimiterOn = isSpeedLimiterOn
+        self.isEngineOn = isEngineOn
+        self.speedLimiterEnabled = speedLimiterEnabled
     }
 }
 
-class trunkCar : MainCar {
-    func action(action: CarAction) {
-        switch action {
-        case .hookTrailer:
-            self.isTrailerHooked = true
-        default:
-            print("Default case was called")
-        }
-    }
-    
-    var currentSpeed: Int = 0
-    
-    var bodyType: String = ""
-    
-    var engineType: String = ""
-    
+class TrunkCar: Car {
+    var isEngineOn: Bool
     var windowOpened: Bool
+    var speed: Int
+    var bodyType: String
+    var isTowed: Bool // Property for TrunkCar only
     
-    var engineIsOn: Bool
-    
-    var selectedGear: Int = 0
-    
-    var isTrailerHooked:Bool // Specific property for this class
-    
-    init(currentSpeed: Int, bodyType: String, engineType: String, windowOpened: Bool, engineIsOn: Bool, selectedGear: Int, isTrailerHooked:Bool ) {
-        self.currentSpeed = currentSpeed
+    init(speed: Int, bodyType:String, windowOpened: Bool, isEngineOn: Bool, isTowed: Bool){
+        self.speed = speed
         self.bodyType = bodyType
-        self.engineType = engineType
         self.windowOpened = windowOpened
-        self.engineIsOn = engineIsOn
-        self.selectedGear = selectedGear
-        self.isTrailerHooked = isTrailerHooked
+        self.isEngineOn = isEngineOn
+        self.isTowed = isTowed
     }
 }
 
-extension SportCar:CustomStringConvertible {
+extension SportCar : CustomStringConvertible {
     var description: String {
-        return "Current Sport Car bodyTypes is: \(self.bodyType)" +
-               " Current speed is \(self.currentSpeed)" +
-               " Current engine type is \(self.engineType)"
+        return "Sport Car extension called"
     }
 }
 
-extension trunkCar: CustomStringConvertible {
+extension TrunkCar : CustomStringConvertible {
     var description: String {
-        return "Current Sport Car bodyTypes is: \(self.bodyType)" +
-               " Current speed is \(self.currentSpeed)" +
-               " Current engine type is \(self.engineType)"
+        return "Trunk Car extension called"
     }
 }
 
-let ferrari = SportCar(currentSpeed: 0, bodyType: "Coupe", engineType: "Gas", windowOpened: false, engineIsOn: false, selectedGear: 1, isSpeedLimiterOn: false)
-print(ferrari.description)
-ferrari.action(action: .turnOnSpeedLimiter)
-print(ferrari.isSpeedLimiterOn)
-ferrari.action(action: .openWindow)
-print(ferrari.windowOpened)
+
+var ferrari = SportCar(speed: 300, bodyType: "Coupe", windowOpened: false, isEngineOn: false, speedLimiterEnabled: true)
+ferrari.openWindow()
+ferrari.windowOpened
+let s = String(describing: ferrari)
+print (s)
+
+var truck = TrunkCar(speed: 20, bodyType: "Pickup", windowOpened: false, isEngineOn: true, isTowed: true)
+print(truck.bodyType)
+truck.openWindow()
+truck.windowOpened
+truck.closeWindow()
+truck.windowOpened
+let truckDescription = String(describing: truck)
+print(truckDescription)
+
+
