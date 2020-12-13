02828fc07bfa4b302f4d911c2823952f02430f00
Denis Mordvinov <denis.mordvinov@corp.badoo.com>
2020-12-13 23:58:05 +0000

    Init Struct with Generics

--- 5l_MordvinovDenis.playground/Contents.swift
+++ 5l_MordvinovDenis.playground/Contents.swift
@@ -1,86 +1,21 @@
-protocol Car    {
-    var speed: Int { get }
-    var bodyType: String { get }
-    var windowOpened:Bool { get set }
-    var isEngineOn: Bool { get set }
-}
+import Foundation
 
-extension Car{
-    mutating func openWindow(){
-        self.windowOpened = true
+struct Queue<Q>{
+    var currentQueue: [Q] = []
+    
+    init(initQueue:[Q]) {
+        self.currentQueue = initQueue
     }
     
-    mutating func closeWindow(){
-        self.windowOpened = false
+    
+    mutating func pushOnCurrentQueue(_ elememt: Q) {
+        currentQueue.append(elememt)
     }
     
-    mutating func startEngine(){
-        self.isEngineOn = true
-    }
-    
-    mutating func stopEngine(){
-        self.isEngineOn = false
+    mutating func removeFromQueue(_ element: Q) {
+        print(element.self)
     }
 }
 
-class SportCar: Car {
-    var isEngineOn: Bool
-    var windowOpened: Bool
-    var speed: Int
-    var bodyType: String
-    var speedLimiterEnabled: Bool // Property for sportCar only
-    
-    init(speed: Int, bodyType:String, windowOpened: Bool, isEngineOn: Bool, speedLimiterEnabled: Bool){
-        self.speed = speed
-        self.bodyType = bodyType
-        self.windowOpened = windowOpened
-        self.isEngineOn = isEngineOn
-        self.speedLimiterEnabled = speedLimiterEnabled
-    }
-}
-
-class TrunkCar: Car {
-    var isEngineOn: Bool
-    var windowOpened: Bool
-    var speed: Int
-    var bodyType: String
-    var isTowed: Bool // Property for TrunkCar only
-    
-    init(speed: Int, bodyType:String, windowOpened: Bool, isEngineOn: Bool, isTowed: Bool){
-        self.speed = speed
-        self.bodyType = bodyType
-        self.windowOpened = windowOpened
-        self.isEngineOn = isEngineOn
-        self.isTowed = isTowed
-    }
-}
-
-extension SportCar : CustomStringConvertible {
-    var description: String {
-        return "Sport Car extension called"
-    }
-}
-
-extension TrunkCar : CustomStringConvertible {
-    var description: String {
-        return "Trunk Car extension called"
-    }
-}
-
-
-var ferrari = SportCar(speed: 300, bodyType: "Coupe", windowOpened: false, isEngineOn: false, speedLimiterEnabled: true)
-ferrari.openWindow()
-ferrari.windowOpened
-let s = String(describing: ferrari)
-print (s)
-
-var truck = TrunkCar(speed: 20, bodyType: "Pickup", windowOpened: false, isEngineOn: true, isTowed: true)
-print(truck.bodyType)
-truck.openWindow()
-truck.windowOpened
-truck.closeWindow()
-truck.windowOpened
-let truckDescription = String(describing: truck)
-print(truckDescription)
-
-
+let queue = Queue(initQueue: [1,2,3,4,5,6,7,8,9,0])
+print(queue.currentQueue)
