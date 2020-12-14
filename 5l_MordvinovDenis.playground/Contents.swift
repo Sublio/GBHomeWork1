5e935896040901239397909614564f2ba11465c2
Denis Mordvinov <denis.mordvinov@corp.badoo.com>
2020-12-14 02:25:04 +0000

    Final version

--- 5l_MordvinovDenis.playground/Contents.swift
+++ 5l_MordvinovDenis.playground/Contents.swift
@@ -1,21 +1,64 @@
 import Foundation
+import UIKit
 
-struct Queue<Q>{
-    var currentQueue: [Q] = []
+struct Element: Equatable {
     
-    init(initQueue:[Q]) {
-        self.currentQueue = initQueue
+//    static func ==(first: Element, second: Element) -> Bool {
+//        return first == second
+//    }
+}
+
+
+struct Queue<Element>{
+    var currentQueueOfElements: [Element] = []
+    
+    init(initWithElements:[Element]) {
+        self.currentQueueOfElements = initWithElements
     }
     
     
-    mutating func pushOnCurrentQueue(_ elememt: Q) {
-        currentQueue.append(elememt)
+    mutating func pushOnCurrentQueue(_ elememt: Element) {
+        currentQueueOfElements.append(elememt)
     }
     
-    mutating func removeFromQueue(_ element: Q) {
-        print(element.self)
+    // This may be optional as currentQueue might be empty
+    mutating func removeLastFromQueue() -> Element?  {
+        return currentQueueOfElements.removeLast()
+    }
+    
+    subscript (index: Int) -> Bool? {
+        if currentQueueOfElements.indices.contains(index){
+          print("Found an element in array")
+          return true
+        }else {
+            print("There is no element in array")
+            return nil
+        }
     }
 }
 
-let queue = Queue(initQueue: [1,2,3,4,5,6,7,8,9,0])
-print(queue.currentQueue)
+ func filterQueue(queue:[Int], predicate: (Int) -> Bool) -> [Int] {
+    var tmpArray = [Int]()
+    
+    for element in queue {
+        if predicate(element){
+            tmpArray.append(element)
+        }
+    }
+    
+    return tmpArray
+    
+}
+
+var queue = Queue(initWithElements: [1,2,3,4,6])
+
+let divideByThree: (Int) -> Bool = { (element: Int) -> Bool in
+    return element % 3 == 0
+}
+
+print(queue.currentQueueOfElements)
+
+filterQueue(queue: queue.currentQueueOfElements, predicate: divideByThree )
+
+let result = queue[5]
+
